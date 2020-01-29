980903a6ec240cd3dc40a6d5742b45017d7eb916
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-01-29 11:35:59 -0500

    pages post, about and index!

---
+++ blocs/pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo nisi quasi fugiat! Labore, debitis enim. Dolores, illum laudantium ut quam tenetur nesciunt exercitationem quis quibusdam voluptate earum veritatis praesentium vitae?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head(){
+    return  {
+      title: 'Application Nuxt Blocs | About',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Website about post'
+        }
+      ],
+      htmlAttrs:{
+        lang: 'en'
+      }
+    }
+  }
+}
+</script>
---
+++ blocs/pages/posts/index.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <div>
+    <h1>Posts</h1>
+
+    <post 
+      v-for='post of posts'
+      :key='post.id'
+      :title='post.title'
+      :id='post.id'
+    />
+  </div>
+
+</template>
+
+<script>
+
+import axios from 'axios'
+import post from '../../components/post'
+
+export default {
+  components:{
+    post
+  },
+  data(){
+    return {
+      posts: []
+    }
+  },
+  async created(){
+    try {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      this.posts = res.data;
+    } catch (error) {
+      console.error(error);
+    }    
+  }
+}
+</script>

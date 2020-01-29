d91ce86afe5ec382f55a230f1aab4ff24b4b3ab9
Percy Maldonado Quispe <maldonadoq.percy@gmail.com>
2020-01-29 11:35:22 -0500

    navigation bar and post component

---
+++ blocs/components/post.vue
@@ -0,0 +1,31 @@
+<template>
+  <nuxt-link :to="'/posts/' + id">
+    <div class="post">    
+      <p>{{id}} - {{title}}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'post',
+  props: ['id', 'title']
+}
+</script>
+
+<style>
+  .post{
+    padding: 1rem;
+    border: 1px solid whitesmoke;
+    margin: 1rem 0;
+  }
+
+  .post:hover{
+    border: 1px solid #6c5ce7;
+    cursor: pointer;
+  }
+
+  a{
+
+  }
+</style>
